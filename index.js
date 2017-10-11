4075215894bc87d6527edee6b927a3246b74c967
Timothy Cevallos <timcev@gmail.com>
2017-10-11 17:25:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+function showRepositories(event, data){
+  conosle.log(this.responseText)
+}
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.open("GET", 'https://api.github.com/users/octocat/repos')
